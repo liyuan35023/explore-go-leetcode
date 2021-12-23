f34b850bc083bec85dc9d7f27db279cfe9ad8b3e
liyuan35023 <liyuan_35023@163.com>
2021-12-23 10:42:25 +0800

    2021-12-23(1)

--- 0-plugin-tmp/leetcode/editor/cn/123-best-time-to-buy-and-sell-stock-iii.go
+++ 0-plugin-tmp/leetcode/editor/cn/123-best-time-to-buy-and-sell-stock-iii.go
@@ -39,33 +39,9 @@ package cn
 
 //leetcode submit region begin(Prohibit modification and deletion)
 func maxProfit(prices []int) int {
-	dp0 := 0                   // 不持有
-	dp1 := -prices[0]          // 第一次持有
-	dp2 := 0                   // 不持有，完成了第一个交易
-	dp3 := -prices[0]          // 第二次持有
-	dp4 := 0                   // 不持有，完成了两次交易
-	for i := 1; i < len(prices); i++ {
-		dp1, dp2, dp3, dp4 = max(dp1, dp0-prices[i]), max(dp2, dp1+prices[i]), max(dp3, dp2-prices[i]), max(dp4, dp3+prices[i])
-	}
-	return max(dp0, max(dp2, dp4))
-}
-//func maxProfit(prices []int) int {
-//	// dp 5种状态
-//	dp := make([][5]int, len(prices))
-//	for i := 0; i < len(prices); i++ {
-//		dp[i] = [5]int{0, 0, 0, 0, 0}
-//	}
-//	dp[0][1] = -prices[0]
-//	dp[0][3] = -prices[0]
-//	for i := 1; i < len(prices); i++ {
-//		dp[i][1] = max(dp[i-1][1], dp[i-1][0]-prices[i])
-//		dp[i][2] = max(dp[i-1][2], dp[i-1][1]+prices[i])
-//		dp[i][3] = max(dp[i-1][3], dp[i-1][2]-prices[i])
-//		dp[i][4] = max(dp[i-1][4], dp[i-1][3]+prices[i])
-//	}
-//	return max(dp[len(prices)-1][0], max(dp[len(prices)-1][2], dp[len(prices)-1][4]))
-//}
 
+
+}
 func max(x, y int) int {
 	if x > y {
 		return x
