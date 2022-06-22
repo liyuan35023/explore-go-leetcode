68e016973edd07b1e208f3dbd8420e401d7d75cc
liyuan35023 <liyuan_35023@163.com>
2022-06-22 12:29:11 +0800

    2022-06-22(2)

--- 0-plugin-tmp/leetcode/editor/cn/160-intersection-of-two-linked-lists.go
+++ 0-plugin-tmp/leetcode/editor/cn/160-intersection-of-two-linked-lists.go
@@ -55,7 +55,20 @@ package cn
  * }
  */
 func getIntersectionNode(headA, headB *ListNode) *ListNode {
-
+	curA, curB := headA, headB
+	for curA != curB {
+		if curA != nil {
+			curA = curA.Next
+		} else {
+			curA = headB
+		}
+		if curB != nil {
+			curB = curB.Next
+		} else {
+			curB = headA
+		}
+	}
+	return curA
 
 }
 //leetcode submit region end(Prohibit modification and deletion)
--- 0-plugin-tmp/leetcode/editor/cn/232-implement-queue-using-stacks.go
+++ 0-plugin-tmp/leetcode/editor/cn/232-implement-queue-using-stacks.go
@@ -51,26 +51,44 @@ type MyQueue struct {
 
 /** Initialize your data structure here. */
 func Constructor() MyQueue {
+	return MyQueue{
+		stack1: make([]int, 0),
+		stack2: make([]int, 0),
+	}
 }
 
 
 /** Push element x to the back of queue. */
 func (this *MyQueue) Push(x int) {
+	for len(this.stack1) != 0 {
+		this.stack2 = append(this.stack2, this.stack1[len(this.stack1)-1])
+		this.stack1 = this.stack1[:len(this.stack1)-1]
+	}
+	this.stack1 = append(this.stack1, x)
+	for len(this.stack2) != 0 {
+		this.stack1 = append(this.stack1, this.stack2[len(this.stack2)-1])
+		this.stack2 = this.stack2[:len(this.stack2)-1]
+	}
 }
 
 
 /** Removes the element from in front of queue and returns that element. */
 func (this *MyQueue) Pop() int {
+	ans := this.stack1[len(this.stack1)-1]
+	this.stack1 = this.stack1[:len(this.stack1)-1]
+	return ans
 }
 
 
 /** Get the front element. */
 func (this *MyQueue) Peek() int {
+	return this.stack1[len(this.stack1)-1]
 }
 
 
 /** Returns whether the queue is empty. */
 func (this *MyQueue) Empty() bool {
+	return len(this.stack1) == 0
 }
 
 
